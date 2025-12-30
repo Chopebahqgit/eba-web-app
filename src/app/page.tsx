efa3329d54b52c23d8afb60b5e5420744a5d2c45
Topsurpass <temitopeabiodun685@gmail.com>
2025-12-30 23:18:26 +0100

    added husky and lintstaged for code complaince

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 export default function Home() {
-	const x = 1;
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<h1>EBA Markeplace</h1>
